0d36eeaee1970b3c52d0de77d60c1f8b9c4ab573
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-10 19:40:58 +0900

    ✨ローカルストレージからのカードデータの設定と取得

--- src/main.js
+++ src/main.js
@@ -17,20 +17,22 @@ let currentActiveCard = 0;
 const cardsEl = [];
 
 // カードのデータを格納
-const cardsData = [
-  {
-    question: "What must a variable begin with?",
-    answer: "A letter, $ or _",
-  },
-  {
-    question: "What is a variable?",
-    answer: "Container for a piece of data",
-  },
-  {
-    question: "Example of Case Sensitive Variable",
-    answer: "thisIsAVariable",
-  },
-];
+const cardsData = getCardsData();
+
+// const cardsData = [
+//   {
+//     question: "What must a variable begin with?",
+//     answer: "A letter, $ or _",
+//   },
+//   {
+//     question: "What is a variable?",
+//     answer: "Container for a piece of data",
+//   },
+//   {
+//     question: "Example of Case Sensitive Variable",
+//     answer: "thisIsAVariable",
+//   },
+// ];
 
 // 全てのカードを作成
 function createCards() {
@@ -74,6 +76,18 @@ function updateCurrentText() {
   currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
+// ローカルストレージからカードを取得
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem("cards"));
+  return cards === null ? [] : cards;
+}
+
+// ローカルストレージにカードを追加
+function setCardsData(cards) {
+  localStorage.setItem("cards", JSON.stringify(cards));
+  window.location.reload();
+}
+
 createCards();
 
 // イベントリスナー
@@ -103,4 +117,40 @@ prevBtn.addEventListener("click", () => {
   cardsEl[currentActiveCard].className = "card active";
 
   updateCurrentText();
+});
+
+// 'Add New Card'ボタンの実装
+showBtn.addEventListener("click", () => {
+  addContainer.classList.add("show");
+});
+
+// 'X'ボタンの実装
+hideBtn.addEventListener("click", () => {
+  addContainer.classList.remove("show");
+});
+
+// 'Add Card'ボタンの実装
+addCardBtn.addEventListener("click", () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question: question, answer: answer };
+    createCard(newCard);
+
+    questionEl.value = "";
+    answerEl.value = "";
+
+    addContainer.classList.remove("show");
+
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+
+// 'Clear Cards'ボタンの実装
+clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = "";
+  window.location.reload();
 });
